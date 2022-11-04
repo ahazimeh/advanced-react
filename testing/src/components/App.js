1579556fd344e704c5201c09d59bbb491de7119c
ahazimeh <hazimeh95@gmail.com>
2022-11-04 15:21:58 +0200

    first commit

---
+++ testing/src/components/App.js
@@ -0,0 +1,13 @@
+import CommentBox from "./CommentBox";
+import CommentList from "./CommentList";
+
+function App() {
+  return (
+    <div className="App">
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+}
+
+export default App;
