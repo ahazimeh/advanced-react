6eebb5507c9ada2e32b7ecf302c1ae9c2bea93c2
ahazimeh <hazimeh95@gmail.com>
2022-11-07 20:51:44 +0200

    waiting for promise resolution

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import async from "middlewares/async";
 import reduxPromise from "redux-promise";
 
 import reducers from "reducers";
-
 export default ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise)) 
+    const store = createStore(reducers, initialState, applyMiddleware(/*reduxPromise*/async)) 
 
     return (
         <Provider store={store}>
