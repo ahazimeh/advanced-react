e7c45c8f88394b7b2b19001cc5880413a9f89e17
ahazimeh <hazimeh95@gmail.com>
2022-11-08 23:26:26 +0200

    code cleanup with compose

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,8 @@
+import axios from "axios";
 import { AUTH_USER } from "./types";
 
 // We can return a function because we installed redux-thunk called with dispatch function
 // With redux thunk we can dispatch multiple requests where using redux-promise we are limited to 1 request
-export const signup = ({ email, password }) => (dispatch) => {
+export const signup = (formProps) => (dispatch) => {
+    axios.post('http://localhost:3090/signup', formProps);
 };
--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -1,9 +1,12 @@
 import { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+import { compose } from "redux";
 
 class Signup extends Component {
     onSubmit = (formProps) => {
-        console.log(formProps)
+        this.props.signup(formProps);
     };
 
     render() {
@@ -35,4 +38,7 @@ class Signup extends Component {
     }
 }
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose(
+    connect(null, actions),
+    reduxForm({ form: 'signup' })
+)(Signup);
