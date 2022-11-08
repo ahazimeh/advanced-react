bcfc6225d9f054d107c0fdfc3719f4024a3d5cc3
ahazimeh <hazimeh95@gmail.com>
2022-11-08 23:48:24 +0200

    solution to cors error

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
-// require('./models/user');
+const cors = require('cors');
 // mongoose.connect('mongodb://localhost:auth/auth');
 mongoose.connect('mongodb://127.0.0.1:27017/test?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0')
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
