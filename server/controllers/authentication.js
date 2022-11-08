2fd543d277856f8fbc8b40b15b76b302f1b0be1e
ahazimeh <hazimeh95@gmail.com>
2022-11-08 12:49:11 +0200

    creating user records

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -6,12 +6,28 @@ exports.signup = function(req, res, next) {
     const password = req.body.password
     // See if a user with a given email exists
     User.findOne({ email }, function(err, existingUser) {
-        
+        if(err) {
+            return next(err);
+        }
+
+        // If a user with email does exist, return an error
+        if(existingUser) {
+            return res.status(422).send({ error: 'Email is in user' });
+        }
+
+        // If a user with email does NOT exist, create and save user record
+        const user = new User({
+            email,
+            password
+        });
+
+        user.save(function(err) {
+            if(err) {
+                return next(err);
+            }
+
+            // Respond to request that indicating the user was created
+            res.json({ success: true });
+        });
     });
-
-    // If a user with email does exist, return an error
-
-    // If a user with email does NOT exist, create and save user record
-
-    // Respond to request that indicating the user was created
 }
