50015fe6f0028aed084d49f876c5e372031dca1d
ahazimeh <hazimeh95@gmail.com>
2022-11-09 10:22:05 +0200

    auth wrapup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom"
+import "./HeaderStyle.css";
 
 class Header extends Component {
     renderLinks() {
@@ -23,7 +24,7 @@ class Header extends Component {
 
     render() {
         return (
-            <div>
+            <div className="header">
                 <Link to="/">Redux Auth</Link>
                 {this.renderLinks()}
             </div>
